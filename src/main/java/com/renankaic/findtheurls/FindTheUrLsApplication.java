b603da08ab081d5585ea88e025c46ee85d9ab356
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-02-01 20:34:38 -0200

    Starting Spring Project

---
+++ src/main/java/com/renankaic/findtheurls/FindTheUrLsApplication.java
@@ -0,0 +1,14 @@
+package com.renankaic.findtheurls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTheUrLsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTheUrLsApplication.class, args);
+	}
+
+}
+
