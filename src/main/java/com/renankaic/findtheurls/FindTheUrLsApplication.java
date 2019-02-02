db6e9a652e0f280d274820595b11258de88eb082
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-02-01 23:06:14 -0200

    Creating some example of URLs

--- src/main/java/com/renankaic/findtheurls/FindTheUrLsApplication.java
+++ src/main/java/com/renankaic/findtheurls/FindTheUrLsApplication.java
@@ -1,13 +1,35 @@
 package com.renankaic.findtheurls;
 
+import java.net.URL;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.renankaic.findtheurls.services.UrlService;
+
 @SpringBootApplication
-public class FindTheUrLsApplication {
+public class FindTheUrLsApplication implements CommandLineRunner{
+	
+	@Autowired
+	UrlService urlService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindTheUrLsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//Instantiate some example objects
+		URL url1 = new URL("https://www.monkkee.com/en/");
+		URL url2 = new URL("https://www.netflix.com/br/");	
+	
+		
+		urlService.saveAll(Arrays.asList(url1, url2));
+		
 	}
 
 }
--- src/main/java/com/renankaic/findtheurls/services/UrlService.java
+++ src/main/java/com/renankaic/findtheurls/services/UrlService.java
@@ -1,5 +1,9 @@
 package com.renankaic.findtheurls.services;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +18,33 @@ public class UrlService {
 	@Autowired
 	private UrlRepository repository;
 	
-	public Url find(Integer id) {
-		
+	public Url find(Integer id) {		
 		Optional<Url> obj = repository.findById(id);
-		return obj.orElse(null);		
+		return obj.orElse(null);	
+	}
+	
+	public Url save(Url obj) {
+		if(  obj != null ) {			
+			repository.save(obj);		
+			return obj;
+		} else {
+			return null;
+		}
+	}
+	
+	public List<Url> saveAll(List<URL> list){
+		
+		List<Url> savedUrls = new ArrayList<Url>();
+		for(URL url: list) {
+			
+			Url obj = new Url();
+			obj.setCreation_date(new Date());
+			obj.setUrl(url);
+			repository.save(obj);
+			savedUrls.add(obj);
+		
+		}
+		return savedUrls;		
 		
 	}
 
